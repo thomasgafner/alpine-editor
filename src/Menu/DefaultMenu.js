082587cf82034d48eddf4ff637ccc6327f8be637
thomas.gafner <thomas.gafner@gmail.com>
2021-05-10 19:06:25 +0200

    Sketch two or three editor instances.

--- src/Menu/DefaultMenu.js
+++ src/Menu/DefaultMenu.js
@@ -2,14 +2,14 @@
 function createActionGroup(name) {
 	let elt = document.createElement("span");
 	elt.classList.add('action-group');
-	elt.classList.add(`${name}-action`);
+	elt.classList.add(`${name}-actions`);
 	return elt;
 }
 
 function createAction(name, label, level = false) {
 	let elt = document.createElement("a");
 	elt.classList.add('operation-action');
-	elt.classList.add(name.replace('_', '-') + (level)?`-${level}`:'');
+	elt.classList.add(`${name.replace('_', '-')}${(level)?`-${level}`:''}-action`);
 	elt.dataset.activeClass = 'active-state';
 	elt.dataset.command = name;
 	if (level) elt.dataset.level = level;
