70b735a045d23c6833d91702fcdc05a081d27743
thomas.gafner <thomas.gafner@gmail.com>
2021-05-12 17:32:32 +0200

    Proper blockquote wrapping.

--- src/Nodes/Nodes.js
+++ src/Nodes/Nodes.js
@@ -4,7 +4,7 @@ import { wrapInList } from "prosemirror-schema-list";
 export default function initNodes(editor) {
     editor.commandsManager.addCommand('heading', {action: setBlockType, activatable: true,});
     editor.commandsManager.addCommand('paragraph', {action: setBlockType, activatable: true,});
-    editor.commandsManager.addCommand('blockquote', {action: wrapIn, activatable: true,});
+    editor.commandsManager.addCommand('blockquote', {action: wrapInList, activatable: true,}); // better than wrapIn
     editor.commandsManager.addCommand('code_block', {action: setBlockType, activatable: true,});
 
     editor.commandsManager.addCommand('bullet_list', { action: wrapInList, hideable: true });
