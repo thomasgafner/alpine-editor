9e139b95827650a37d381b1ef18080a93e71e662
thomas.gafner <thomas.gafner@gmail.com>
2021-05-16 16:42:17 +0200

    Check and adjust position to be within viewport.

--- src/Menu/ToSelectionPositioning.js
+++ src/Menu/ToSelectionPositioning.js
@@ -13,4 +13,26 @@ export function positionToHead(elt, editorView) {
 	// console.log('x, y', cx, cy)
 	elt.style.left = `${cx}px`;
 	elt.style.top = `${cy}px`;
+
+	// check and adjust position to be within viewport
+	box = elt.getBoundingClientRect();
+	const ih = window.innerHeight || document.documentElement.clientHeight;
+	if (box.bottom > ih) {
+		// Bottom is out of viewport
+		elt.style.top = `${cy - (box.bottom - ih)}px`;
+	}
+	const iw = window.innerWidth || document.documentElement.clientWidth;
+	if (box.right > iw) {
+		// Right side is out of viewport
+		elt.style.left = `${cx - (box.right - iw)}px`;
+	}
+	if (box.top < 0) {
+		// Top is out of viewport
+		elt.style.top = '0px';
+	}
+	if (box.left < 0) {
+		// Left side is out of viewoprt
+		elt.style.left = '0px';
+	}
+
 }
